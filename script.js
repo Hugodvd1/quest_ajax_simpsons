48946eed938f86b82a59b6d3759f2d1670383d43
Hugo David <hugodvd@outlook.com>
2022-11-15 06:59:26 +0100

    first commit

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 const btnChange = document.getElementById('change');
 
 
-// This function loads simpson data from the Pokemon API
 function fetchSimpsonJSON() {
     const url = `https://simpsons-quotes-api.herokuapp.com/quotes`;
     axios.get(url)
-      .then(function(response) {
-        return response.data; // response.data instead of response.json() with fetch
-      })
-      .then(function(simpson) {
-        console.log('data decoded from JSON:', simpson[0]);
-  
-        // Build a block of HTML
-        const simpsonHtml = `
+        .then(function (response) {
+            return response.data; 
+        })
+        .then(function (simpson) {
+            console.log('data decoded from JSON:', simpson[0]);
+
+            const simpsonHtml = `
           <p><strong>${simpson[0].character}</strong></p>
           <img src="${simpson[0].image}" />
           <p>${simpson[0].quote}</p>
         `;
-        document.querySelector('#simpson-quote').innerHTML = simpsonHtml;
-      });
-  }
+            document.querySelector('#simpson-quest').innerHTML = simpsonHtml;
+        });
+}
 
-  fetchSimpsonJSON();
+fetchSimpsonJSON();
 
-  btnChange.addEventListener('click', function () {
+btnChange.addEventListener('click', function () {
     fetchSimpsonJSON();
-  });
+});
